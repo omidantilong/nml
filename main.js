e9a2f94a1fbd548345453bffc9b62f0970f1526a
Omid Kashan <omid@antilong.com>
2022-10-09 20:23:36 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+import './style.css'
+import {convertXML, createAST} from "simple-xml-to-json"
+
+const inputElement = document.querySelector('#files')
+const output = document.querySelector('#output')
+
+inputElement.addEventListener("change", handleFiles, false);
+function handleFiles() {
+  const fileList = this.files;
+  const reader = new FileReader();
+  reader.onload = function(e) {
+    const data = convertXML(e.target.result);
+    const { ENTRIES: entries, children } = data.NML.children[2].COLLECTION
+    const html = []
+
+    console.log(entries, children) 
+
+    children.forEach(entry => {
+      const {ARTIST: artist, TITLE: title} = entry.ENTRY
+      html.push(`<tr><td>${artist}</td><td>${title}</td></tr>`)
+    })
+
+    output.innerHTML = html.join('')
+
+  }
+
+  reader.readAsText(fileList[0]);
+}
+

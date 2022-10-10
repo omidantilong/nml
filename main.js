e1e8cac9a0b23b1502e1bc8ade85fed54adfbf2e
Omid Kashan <omid@antilong.com>
2022-10-10 09:08:57 +0100

    add drag+drop, filename, bpm

--- main.js
+++ main.js
@@ -1,31 +1,69 @@
-import './style.css'
+import "./style.css"
 import { convertXML } from "simple-xml-to-json"
 
-const inputElement = document.querySelector('#files')
-const output = document.querySelector('#output')
+const inputElement = document.querySelector("#files")
+const output = document.querySelector("#output tbody")
+const body = document.querySelector("body")
+const filename = document.querySelector("#filename")
 
-inputElement.addEventListener("change", handleFiles, false);
+body.addEventListener("dragenter", dragenter, false)
+body.addEventListener("dragover", dragover, false)
+body.addEventListener("drop", drop, false)
 
-function handleFiles() {
+inputElement.addEventListener(
+  "change",
+  function () {
+    handleFiles(this.files[0])
+  },
+  false
+)
 
-  const fileList = this.files;
-  const reader = new FileReader();
-  
-  reader.onload = function(e) {
+function handleFiles(file) {
+  const reader = new FileReader()
 
-    const data = convertXML(e.target.result);
-    const { ENTRIES: entries, children } = data.NML.children[2].COLLECTION
+  reader.onload = function (e) {
+    const playlist = convertXML(e.target.result)
+    const { ENTRIES: entries, children } = playlist.NML.children[2].COLLECTION
     const html = []
+    console.log(playlist)
+    children.forEach((entry) => {
+      const track = entry.ENTRY
+      const { ARTIST: artist, TITLE: title } = track
+      const { TEMPO: tempo } = track.children[3]
+      const { ALBUM: album } = track.children[1]
 
-    children.forEach(entry => {
-      const {ARTIST: artist, TITLE: title} = entry.ENTRY
-      html.push(`<tr><td>${artist}</td><td>${title}</td></tr>`)
+      const bpm = tempo?.BPM || "-"
+      const albumTitle = album?.TITLE || "-"
+      html.push(
+        `<tr><td>${artist}</td><td>${title}</td><td>${albumTitle}</td><td>${bpm}</td></tr>`
+      )
     })
-
-    output.innerHTML = html.join('')
-
+    filename.textContent = file.name
+    output.innerHTML = html.join("")
+    body.classList.add("loaded")
   }
 
-  reader.readAsText(fileList[0]);
+  reader.readAsText(file)
 }
 
+function drop(e) {
+  e.stopPropagation()
+  e.preventDefault()
+
+  const dt = e.dataTransfer
+  const files = dt.files
+
+  inputElement.value = ""
+
+  handleFiles(files[0])
+}
+
+function dragenter(e) {
+  e.stopPropagation()
+  e.preventDefault()
+}
+
+function dragover(e) {
+  e.stopPropagation()
+  e.preventDefault()
+}

70056faf009342f6d5d73f51bb53f916c84dfb50
Omid Kashan <omid@antilong.com>
2022-10-10 19:47:17 +0100

    add csv download

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 import "./style.css"
 import { convertXML } from "simple-xml-to-json"
+import { json2csvAsync } from "json-2-csv"
 
 const inputElement = document.querySelector("#files")
 const output = document.querySelector("#output")
 const body = document.querySelector("body")
 const filename = document.querySelector("#filename")
+const master = {}
+const thead = `<thead><tr>
+    <th>#</th>
+    <th>Artist</th>
+    <th>Track</th>
+    <th>Album</th>
+    <th>BPM</th>
+    <th>File</th>
+  </tr></thead>`
 
 body.addEventListener("dragenter", dragenter, false)
 body.addEventListener("dragover", dragover, false)
@@ -17,17 +27,11 @@ body.addEventListener("click", (e) => {
     if (details) {
       details.remove()
     }
+  } else if (e.target.classList.contains("download")) {
+    const id = e.target.dataset.id
+    downloadCsv(master[id])
   }
 })
-
-const thead = `<thead><tr>
-    <th>#</th>
-    <th>Artist</th>
-    <th>Track</th>
-    <th>Album</th>
-    <th>BPM</th>
-    <th>File</th>
-  </tr></thead>`
 
 inputElement.addEventListener(
   "change",
@@ -36,6 +40,20 @@ inputElement.addEventListener(
   },
   false
 )
+
+function downloadCsv({ file, data }) {
+  json2csvAsync(data)
+    .then((csv) => {
+      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" })
+      const url = URL.createObjectURL(blob)
+      const link = document.createElement("a")
+      link.setAttribute("href", url)
+      link.setAttribute("download", file + ".csv")
+      link.click()
+      link.remove()
+    })
+    .catch((err) => console.log("ERROR: " + err.message))
+}
 
 function iterateFiles(files) {
   for (let i = 0; i < files.length; i++) {
@@ -50,12 +68,16 @@ function handleFile(file) {
     const playlist = convertXML(e.target.result)
     const { ENTRIES: count, children } = playlist.NML.children[2].COLLECTION
     const html = []
+    const data = []
     const uuid = crypto.randomUUID()
     html.push(
       `<details data-id="${uuid}">
         <summary>
           <h2>${file.name} (${children.length})</h2>
-          <button class="remove" data-id="${uuid}">Remove</button>
+          <div class="actions">
+            <button class="download" data-id="${uuid}">Download CSV</button>
+            <button class="remove" data-id="${uuid}">Remove</button>
+          </div>
         </summary>
         <table width="100%">
           ${thead}
@@ -63,6 +85,7 @@ function handleFile(file) {
     )
 
     console.log(playlist)
+
     children.forEach((entry, i) => {
       const track = entry.ENTRY
       const { ARTIST: artist, TITLE: title } = track
@@ -80,13 +103,14 @@ function handleFile(file) {
           meta.location = child.LOCATION
         }
       })
-      console.log(meta)
 
-      values.bpm = meta?.tempo?.BPM || "-"
-      values.album = meta?.album?.TITLE || "-"
       values.artist = artist && artist !== "undefined" ? artist : "-"
       values.title = title
+      values.album = meta?.album?.TITLE || "-"
+      values.bpm = meta?.tempo?.BPM || "-"
       values.file = meta?.location?.FILE || "-"
+
+      data.push(values)
 
       html.push(
         `<tr>
@@ -100,9 +124,9 @@ function handleFile(file) {
       )
     })
     html.push("</tbody></table></details>")
-    console.log(html)
     output.innerHTML += html.join("")
     body.classList.add("loaded")
+    master[uuid] = { file: file.name, data }
   }
 
   reader.readAsText(file)
